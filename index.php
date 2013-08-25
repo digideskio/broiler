29909f935370efa3bc420bc76bffedd93e2aabf3
Bastian Allgeier <bastian.allgeier@gmail.com>
2013-08-25 22:38:14 +0200

    first version

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8" />  
+    
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow" />
+
+    <title>Your Project</title>
+
+    <link rel="stylesheet" href="assets/css/site.css">
+
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+
+    
+    <script type="text/javascript" src="assets/js/jquery.js"></script>
+
+  </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Broiler
+
+This is a very reduced boilerplate, which includes my most beloved assets folder structure, the SASSAS framework and some basic html. 
+
+## Installation
+
+		git clone --recursive https://github.com/bastianallgeier/broiler.git your-site-name
+
+Replace your-site-name of course :)
+
+## License
+MIT License <http://www.opensource.org/licenses/mit-license.php>
